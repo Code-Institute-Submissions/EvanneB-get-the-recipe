7136db0e1366c1d11466cde286e576bfb2956450
EvanneB <evannebreslin@hotmail.com>
2020-09-16 22:31:51 +0000

    CSS changes

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,61 +1,42 @@
-// -----------------------------------------------  Selectors
-const searchButton = document.querySelector("#search");
-const recipeCards = document.querySelector("#content");
-const clearIcon = document.querySelector("#recipe-form__clear-icon");
-const searchBar = document.querySelector("#recipe-form__search-bar");
-
-// ----------------------------------------------- Event Listeners
-searchButton.addEventListener("click", () => {
-    recipeAPI();
+const searchBtn = document.querySelector("#searchBtn");
+const recipes = document.querySelector("#content");
+const search = document.querySelector("#search_recipes");
+​
+searchBtn.addEventListener("click", () => {
+    enquireTheRecipeAPI();
 });
-
-//Event listeners taken from https://www.mikedane.com/web-development/css/styling-search-bar/
-searchBar.addEventListener("keyup", () => {
-    if(searchBar.value && clearIcon.style.visibility != "visible"){
-        clearIcon.style.visibility = "visible";
-    } else if(!searchBar.value) {
-        clearIcon.style.visibility = "hidden";
-    }
-});
-
-clearIcon.addEventListener("click", () => {
-    searchBar.value = "";
-    clearIcon.style.visibility = "hidden";
-});
-
-// ----------------------------------------------- Recipe API
-let appId = '187461a3',
-    apiKey = '40fec7274b46ff61bd3cfad60ed50534';
-// ^^ get your own keys! from edamam.com
-
-// Get data from Recipe API
-async function recipeAPI() {
-    let searchValue = document.querySelector("#recipe-form__search-bar").value;
-    let response = await fetch(`https://api.edamam.com/search?app_id=${appId}&app_key=${apiKey}&q=${searchValue}`)
+​
+​
+const appId = '187461a3';
+const apiKey = '40fec7274b46ff61bd3cfad60ed50534';
+​
+async function enquireTheRecipeAPI() {
+    let entered_search = document.querySelector("#search_recipes").value;
+    let response = await fetch(`https://api.edamam.com/search?app_id=${appId}&app_key=${apiKey}&q=${entered_search}`)
         .then(response => response.json())
         .then(data => {
-            recipeAPIData(data);
+            getTheRecipeDetails(data);
         })
-}​
-// Use data from Recipe API 
-function recipeAPIData(data) {
-    for (let i = 0; i < 8; i++) {
-        recipeCards.innerHTML += `
+}
+​
+function getTheRecipeDetails(data) {
+    recipes.innerHTML = "";
+    for (let counter = 0; counter < 12; counter++) {
+        recipes.innerHTML += `
     <div class="col-12 col-md-6 col-xl-3 col-lg-3">
         <div class="card">
-        <img src="${data.hits[i].recipe.image}"
+        <img src="${data.hits[counter].recipe.image}"
             class="card-img-top" alt="Recipe image">
         <div class="card-body">
-            <h5 class="card-title" id="card-title">${data.hits[i].recipe.label}</h5>
+            <h5 class="card-title" id="card-title">${data.hits[counter].recipe.label}</h5>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">Calories: ${parseInt(data.hits[i].recipe.calories)}</li>
-            <li class="list-group-item">Ingredients used: ${data.hits[i].recipe.ingredients.length}</li>
-            <li class="list-group-item">Health labels: ${data.hits[i].recipe.healthLabels}</li>
-            <li class="list-group-item">Diet labels: ${data.hits[i].recipe.dietLabels}</li>
+            <li class="list-group-item">Ingredients used: ${data.hits[counter].recipe.ingredients.length}</li>
+            <li class="list-group-item">Calories: ${parseInt(data.hits[counter].recipe.calories)}</li>        
         </ul>
-        <a href="${data.hits[i].recipe.url}" target="_blank" class="btn btn-primary">See Recipe</a>
+        <a href="${data.hits[counter].recipe.url}" target="_blank" class="btn btn-success">See Recipe</a>
         </div>
+        <hr />
     </div>
         `
     }
